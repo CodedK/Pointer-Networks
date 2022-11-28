33305b024803e82364ff19541c70e139b2210f0e
Panagiotis Kalatzantonakis <kenny22a@hotmail.com>
2022-11-28 17:20:51 +0200

    Mon.17.20

--- generate_data.py
+++ generate_data.py
@@ -13,9 +13,9 @@ def generate_single_seq(length=30, min_len=5, max_len=10):
     min_len: minimum length of sequence
     max_len: maximum length of sequence
     Output: Sequence of numbers, index of the start of greater numbers subsequence"""
-    seq_before = [(random.randint(1, 5)) for x in range(random.randint(min_len, max_len))]
-    seq_during = [(random.randint(6, 10)) for x in range(random.randint(min_len, max_len))]
-    seq_after = [random.randint(1, 5) for x in range(random.randint(min_len, max_len))]
+    seq_before = [(random.randint(1, 5)) for _ in range(random.randint(min_len, max_len))]
+    seq_during = [(random.randint(6, 10)) for _ in range(random.randint(min_len, max_len))]
+    seq_after = [random.randint(1, 5) for _ in range(random.randint(min_len, max_len))]
     seq = seq_before + seq_during + seq_after
     seq = seq + ([0] * (length - len(seq)))
     return (seq, len(seq_before), len(seq_before) + len(seq_during)-1)
@@ -34,7 +34,7 @@ def generate_set_seq(N):
     data = []
     starts = []
     ends = []
-    for i in range(N):
+    for _ in range(N):
         seq, ind_start, ind_end = generate_single_seq()
         data.append(seq)
         starts.append(ind_start)
